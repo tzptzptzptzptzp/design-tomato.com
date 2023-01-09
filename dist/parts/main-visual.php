be577635441ded314f3eb5e3983a7cc54410782a
tzptzptzptzptzp <tzp2539@gmail.com>
2023-01-09 17:38:57 +0900

    ✨ メインビジュアルを動画から画像に変更

--- dist/parts/main-visual.php
+++ dist/parts/main-visual.php
@@ -1,7 +1,12 @@
 <div class="p-main-visual">
     <div class="p-main-visual__inner">
         <div class="p-main-visual__background">
-            <video class="p-main-visual__video" playsinline autoplay loop muted data-src-pc="<?php echo get_template_directory_uri(); ?>/assets/videos/tomato.mp4" data-src-sp="<?php echo get_template_directory_uri(); ?>/assets/videos/tomato_sp.mp4" data-poster-pc="<?php echo get_template_directory_uri(); ?>/assets/videos/poster.jpg" data-poster-sp="<?php echo get_template_directory_uri(); ?>/assets/videos/poster_sp.jpg" width="1920" height="1080"></video>
+            <picture>
+                <source srcset="<?php echo get_template_directory_uri(); ?>/assets/images/mainvisual/poster_sp.webp" media="(max-width: 599px)" type="image/webp"/>
+                <source srcset="<?php echo get_template_directory_uri(); ?>/assets/images/mainvisual/poster_sp.png" media="(max-width: 599px)"/>
+                <source srcset="<?php echo get_template_directory_uri(); ?>/assets/images/mainvisual/poster.webp" type="image/webp"/>
+                <img class="p-main-visual__image" src="<?php echo get_template_directory_uri(); ?>/assets/images/mainvisual/poster.png" width="1600" height="800" alt="メインビジュアル"/>
+            </picture>
         </div>
         <div class="p-main-visual__content">
             <picture>
@@ -17,4 +22,3 @@
         </div>
     </div>
 </div><!-- .p-main-visual -->
-<script type="module" async>let c=window.innerHeight<window.innerWidth||959<=window.innerWidth;let v=document.querySelector(".p-main-visual__video");let s=null;let p=null;if(c){s=v.dataset.srcPc;p=v.dataset.posterPc}else{s=v.dataset.srcSp;p=v.dataset.posterSp};addEventListener("DOMContentLoaded",function(){v.poster=p});addEventListener("load",function(){v.src=s})</script>
